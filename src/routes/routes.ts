2f7177b4dd8058a0a1814ec6024f02ecf93dbba6
jignesh <jvupadhyay137@gmail.com>
2023-08-11 20:17:24 +0530

    feat: project structure

---
+++ src/routes/routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const api = Router().use();
+
+export default Router().use("/api", api);
---
+++ src/utils/http-exception.model.ts
@@ -0,0 +1,13 @@
+class HttpException extends Error {
+  errorCode: number;
+
+  constructor(
+    errorCode: number,
+    public readonly message: string | any
+  ) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+}
+
+export default HttpException;
