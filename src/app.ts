46f54e74dc8bf84c2a0bde7e46f1d45f59e21140
jignesh <jvupadhyay137@gmail.com>
2023-08-18 11:19:25 +0530

    feat: all sign in apis

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(routes);
+app.use(routes);
 
 // Serves images
 // app.use(express.static('public'));
